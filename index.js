f9e2e4290eae3e99c1fdfa3aeba458aa9099893e
Yuya Hirano <v322sk@gmail.com>
2022-09-22 19:34:10 -0700

    fix js

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-// const button = document.getElementById("addBtn");
-// const lists = document.getElementById("lists");
-
-// async function getUsers() {
-//   const res = await fetch("https://jsonplaceholder.typicode.com/users");
-//   const users = await res.json();
-//   return users;
-// }
-
-// async function listUsers() {
-//   //data
-//   const users = await getUsers();
-
-//   //dom
-//   users.forEach(addList);
-// }
-
-// function addList(user) {
-//   const list = document.createElement("li");
-//   list.innerText = user.name;
-//   lists.appendChild(list);
-// }
-
-// window.addEventListener("load", listUsers);
-// button.addEventListener("click", listUsers);
 
 const button = document.getElementById("generateBtn");
 const comment = document.getElementById("commentValue");
